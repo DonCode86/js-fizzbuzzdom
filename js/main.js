d863d8bd05e4d3f159f7f67bb61245fe7414a705
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-02 15:45:50 +0200

    parte style

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
+const list = document.querySelector(".list");
+var element = document.getElementsByClassName("container");
 //stampa in console numeri da 1 a 100
 for( let i = 1; i <= 100; i++ ) {
+    const element = document.createElement("li");
+    element.classList.add("item");
+    element.innerText = i;
+    list.append(element);
+
     if ((i % 5) == 0 && (i % 3) == 0) {
         console.log("fizzbuzz");
     }
     else if ((i % 3) == 0) {
+        element.classList.add("item-three-active");
         console.log("fizz");
     }
     else if ((i % 5) == 0) {
